ac14d37c1b0a974e7c30b61c9bb6f12a3e235207
Jose <jose.veliz@rappi.com>
2022-12-27 18:43:37 -0400

    EMO-1 🎉 Begin emoji-ducen project
    Begin and teste the project to create semantics commits

---
+++ index.js
@@ -0,0 +1,56 @@
+import inquirer from "inquirer";
+import { emojis } from "./emojis.js";
+import inquirerPrompt from "inquirer-autocomplete-prompt";
+import { execa } from "execa";
+
+inquirer.registerPrompt("autocomplete", inquirerPrompt);
+
+const filter = (prompt = "") => {
+  const filtered = emojis.filter((emoji) => emoji.description.includes(prompt));
+  return filtered.map((emoji) => `${emoji.emoji} ${emoji.description}`);
+};
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "project",
+      message: "¿Cual es el código del proyecto?",
+    },
+    {
+      type: "input",
+      name: "number",
+      message: "¿Cual es el numero de la tarea?",
+    },
+    {
+      type: "autocomplete",
+      name: "type",
+      message: "¿Que tipo de adición es?",
+      source: (answersSoFar, input) => filter(input),
+    },
+    {
+      type: "input",
+      name: "title",
+      description: "Titulo del commit",
+    },
+    {
+      type: "input",
+      name: "description",
+      description: "Descripción del commit",
+    },
+  ])
+  .then(async (answers) => {
+    const emoji = answers.type.split(" ")[0];
+    const code = `${answers.project?.toUpperCase() || "TASK"}-${
+      answers.number
+    }`;
+    let title = [code, emoji, answers.title];
+    console.log(title);
+    await execa("git", [
+      "commit",
+      "-m",
+      title.join(" "),
+      "-m",
+      answers.description,
+    ]);
+  });
